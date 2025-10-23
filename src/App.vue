56a9705c20c0bc30ee0e8f80b903e87af8fdc41b
Jonathan Obrian <jonathanobrian132@gmail.com>
2025-10-23 01:20:04 -0600

    Cambio de importación de layout a app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import AppHeader from './components/layouts/AppHeader.vue';
 import AppFooter from './components/layouts/AppFooter.vue';
 
 </script>
 
 <template>
   <div id="app">
+    <AppHeader />
     <router-view />
     <AppFooter />
   </div>
